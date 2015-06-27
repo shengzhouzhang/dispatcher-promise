22a4c98056edc0c518b30d77d7e2bd535e0fd687
Steven Zhang <steven.shengzhou@gmail.com>
2015-06-27 15:22:38 +1000

    remove lodash and bluebird

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,6 +1,4 @@
 
-import _ from 'lodash';
-import Promise from 'bluebird';
 import Debug from 'debug';
 
 let _handlers = {};
@@ -16,23 +14,27 @@ function addHandler (action, handler) {
 function removeHandler (action, handler) {
   debug(`remove handler`);
   if(typeof action !== 'string') { throw new InvalidAction(); }
-  if(!_handlers[action]) { return; }
-  _.remove(_handlers[action], fn => fn === handler);
+  let handlers, index;
+  if(!(handlers = _handlers[action]) ||
+     (index = handlers.indexOf(handler)) === -1) {
+    return;
+  }
+  handlers.splice(index, 1);
 };
 
 function dispatch (action, options) {
   debug(`dispatch ${action}`);
   if(typeof action !== 'string') { throw new InvalidAction(); }
   return Promise.all(
-    _.map(_handlers[action],
+    _handlers[action].map(
       handler => new Promise((resolve, reject) => {
         return handler(resolve, reject, options);
       })
     )
   )
   .then(results => {
-    if (results.length === 0) { return null; }
-    else if (results.length === 1) { return _.first(results); }
+    if (!results || !results.length) { return null; }
+    else if (results.length === 1) { return results.pop(); }
     else { return results; }
   });
 };
--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var Promise     = require('bluebird');
 var assert      = require('assert');
 var debug       = console.log;
 var dispatcher  = require('../build/dispatcher');
